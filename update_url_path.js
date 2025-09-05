f886ccfb68012b8e67efc568bd44bb5876af5f5a
harsh <harshkothari2909@gmail.com>
2025-09-05 14:35:27 +0530

    first commit

--- update_url_path.js
+++ update_url_path.js
@@ -1,15 +1,11 @@
-function image_url(type, filename) {
-  if (!filename) return "";
-  let folder = "";
-  if (type === "destinationCategory") folder = "dastinationcategory"; // <-- spelling match karo
-  if (type === "hotel_syt") folder = "hotel_syt";
-  if (type === "room_syt") folder = "room_syt";
-  if (type === "placephoto") folder = "placephoto";
-  if (type === "vendor_car") folder = "vendor_car";
-  if (type === "car_syt") folder = "car_syt";
-  if (type === "blogger") folder = "blogger";
-  if (type === "safetyinfo") folder = "safetyinfo";
-  return `/public/images/${folder}/${filename}`;
+async function update_path(foldername, data) {
+  if (!data || data.length === 0) {
+    return null;
+  }
+  var updated_data;
+  updated_data = "https://start-your-tour-harsh.onrender.com/images/" + foldername + "/" + data;
+  // console.log(updated_data);
+  return updated_data;
 }
 
-module.exports = image_url;
+module.exports = update_path;
