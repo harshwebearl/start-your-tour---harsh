58462b216ed4d7d4400d53d23bf10fcbd7da74f2
harsh <harshkothari2909@gmail.com>
2025-09-19 14:26:06 +0530

    update selected destination add data3

--- routes/Destination.Route.js
+++ routes/Destination.Route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const destinationnameController = require("../controllers/destination.controller.js");
+const destinationnameController = require("../controllers/destination.controller");
 const DestinationController = new destinationnameController();
 
 router.post("/", (req, res) => DestinationController.RegisterDestination(req, res));
